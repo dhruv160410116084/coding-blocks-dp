8eb8964a36fcf18406cc81358e49bdef441a2377
dhruv <dhruv.160410116084@gmail.com>
2020-04-15 21:18:25 +0530

    improvement is done

---
+++ _.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Integer.*;
+import static java.lang.Math.*;
+import static java.lang.String.*;
+
+
+public class _ {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            int T = parseInt(br.readLine()); 
+            while(T-- != 0){
+                int N = parseInt(br.readLine());
+                StringTokenizer st = new StringTokenizer(br.readLine());
+            }
+            bw.flush();
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
