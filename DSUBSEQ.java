8b3f538358b979dd8b5e677b439253be922376a5
dhruv <dhruv.160410116084@gmail.com>
2020-04-17 14:30:36 +0530

    now i understand whole login for subsequences

--- DSUBSEQ.java
+++ DSUBSEQ.java
@@ -6,8 +6,7 @@ import static java.lang.Math.*;
 import static java.lang.String.*;
 import static java.util.Arrays.*;
 
-
-public class _ {
+public class DSUBSEQ {
     static int mod = 1000000007;
     public static void main(String[] args) {
         try {
@@ -15,8 +14,10 @@ public class _ {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
             int T = parseInt(br.readLine()); 
             while(T-- != 0){
-                int N = parseInt(br.readLine());
-                StringTokenizer st = new StringTokenizer(br.readLine());
+               String str = br.readLine();
+               char arr[] =str.toCharArray();
+               int dp[] = new int[arr.length+1];
+               bw.write(solve(arr,dp)+"\n");
             }
             bw.flush();
         } catch (Exception e) {
@@ -25,20 +26,19 @@ public class _ {
         }
     }
 
-    static int[] takeInputArray(String str, int len) {
-        StringTokenizer st = new StringTokenizer(str);
-        int arr[] = new int[len];
-        for (int i = 0; i < len; i++) {
-            arr[i] = parseInt(st.nextToken());
+    static int solve(char arr[] ,int dp[]){
+        dp[0]=1;
+        int len = arr.length;
+        int lastIdx[] = new int[26];
+        fill(lastIdx,-1);
+       ;
+        for(int i=1;i<=len;i++){
+            dp[i] = (2*dp[i-1])%mod;
+            if(lastIdx[ (arr[i-1] - 'A')%26 ] !=-1 )
+                dp[i] = ( ( dp[i] %mod ) - (dp[ lastIdx[(arr[i-1] - 'A')%26]] % mod) +mod ) % mod;
+            lastIdx[(arr[i-1]-'A')%26] = (i-1);
+
         }
-        return arr;
-    }
-    static long[] takeInputArrayLong(String str, int len) {
-        StringTokenizer st = new StringTokenizer(str);
-        long arr[] = new long[len];
-        for (int i = 0; i < len; i++) {
-            arr[i] = parseLong(st.nextToken());
-        }
-        return arr;
+        return dp[len];
     }
 }
